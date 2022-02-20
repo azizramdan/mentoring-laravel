d32327d396de9f88389dfc76156c9da0b5f0592c
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2022-02-20 14:37:08 +0700

    penerapan jumlah relasi

--- app/Http/Controllers/LandingpageController.php
+++ app/Http/Controllers/LandingpageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -11,9 +12,11 @@ class LandingpageController extends Controller
     {
         $category = $request->category;
         $search = $request->search;
+        $sort = $request->sort;
+        $sortOrder = $request->sort_order;
 
         $products = Product::where('stock', '>', 0)
-            ->with('category');
+            ->with(['category']);
 
         if ($category) {
             $products->where('category_id', $category);
@@ -21,6 +24,19 @@ class LandingpageController extends Controller
 
         if ($search) {
             $products->where('name', 'LIKE', "%$search%");
+        }
+
+        if ($sort) {
+            if ($sort == 'price' || $sort == 'stock') {
+                $products->orderBy($sort, $sortOrder);
+            } elseif ($sort == 'terlaris') {
+                $products->withCount([
+                    'orders' => function ($query) {
+                        $query->where('status', Order::STATUS_SELESAI);
+                    }
+                ])
+                ->orderBy('orders_count', $sortOrder);
+            }
         }
 
         $products = $products->paginate(9);
--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -6,6 +6,23 @@
 <form action="/" method="get">
     <input type="hidden" name="category" value="{{ request()->category }}">
     <input type="text" name="search" class="form-control" placeholder="Masukkan kata kunci" value="{{ request()->search }}">
+
+    <div class="mb-3">
+        <label for="sort" class="form-label">Urutkan</label>
+        <select class="custom-select" name="sort">
+            <option value="">Tidak di urutkan</option>
+            <option value="price">Price</option>
+            <option value="stock">Stock</option>
+            <option value="terlaris">Terlaris</option>
+        </select>
+        <select class="custom-select" name="sort_order">
+            <option value="">Tidak di urutkan</option>
+            <option value="asc">A-Z</option>
+            <option value="desc">Z-A</option>
+        </select>
+    </div>
+
+    <button class="btn btn-primary">Submit</button>
 </form>
 
 <div class="row row-cols-1 row-cols-md-3 g-4 mt-4">
@@ -19,6 +36,7 @@
                 <p class="card-text">{{ $product->description ?? '-' }}</p>
                 <h6><b>Rp. {{ number_format($product->price) }}</b></h6>
                 <h6>Stok: {{ $product->stock }}</h6>
+                <h6>Terjual: {{ $product->orders_count }}</h6>
                 <div class="d-grid">
                     <a href="/checkout/{{ $product->id }}" class="btn btn-primary mt-2">Beli</a>
                 </div>
