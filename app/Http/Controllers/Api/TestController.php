960a634d45be3883e05393d7426817943c87fd8b
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2022-02-28 20:01:31 +0700

    http client / guzzle

---
+++ app/Http/Controllers/Api/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        $response = Http::get('https://quran-endpoint.vercel.app/quran');
+
+        dd($response->collect());
+    }
+}
